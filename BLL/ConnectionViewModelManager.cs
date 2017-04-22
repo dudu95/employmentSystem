52938d5b5c8d76782154037c9aeadea5dacff832
dudu95 <ai804671397@qq.com>
2017-04-22 11:56:07 +0800

    add connectionViewModel and png

---
+++ BLL/ConnectionViewModelManager.cs
@@ -0,0 +1,28 @@
+﻿using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ConnectionViewModelManager
+    {
+        public List<ConnectionViewModel> GetConnectionViewModelList(string condition = "and 1=1 ")
+        {
+            return new ConnectionViewModelService().GetConnectionViewModelList(condition);
+        }
+        public List<ConnectionViewModel> GetConnectionViewModelListByEmployeeId(int employeeId)
+        {
+            string condition = "and con.employeeId = " + employeeId;
+            return new ConnectionViewModelService().GetConnectionViewModelList(condition);
+        }
+        public List<ConnectionViewModel> GetConnectionViewModelListByEmployerId(int employerId)
+        {
+            string condition = "and con.employerId = " + employerId;
+            return new ConnectionViewModelService().GetConnectionViewModelList(condition);
+        }
+    }
+}
